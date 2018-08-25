47e6e6efd6a143d16b81f1b3a051f91a3ceb7a11
Jimin Park <pjm950@gmail.com>
2018-08-26 01:12:36 +0900

    Added a very basic skeleton for MeasureEditForm.

--- OneCharter/MeasureEditForm.Designer.cs
+++ OneCharter/MeasureEditForm.Designer.cs
@@ -23,20 +23,49 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbMeasureType = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Measure Size:";
+            // 
+            // cbMeasureType
+            // 
+            this.cbMeasureType.FormattingEnabled = true;
+            this.cbMeasureType.Items.AddRange(new object[] {
+            "in milliseconds",
+            "in # of beats"});
+            this.cbMeasureType.Location = new System.Drawing.Point(150, 6);
+            this.cbMeasureType.Name = "cbMeasureType";
+            this.cbMeasureType.Size = new System.Drawing.Size(132, 20);
+            this.cbMeasureType.TabIndex = 1;
+            this.cbMeasureType.SelectedIndexChanged += new System.EventHandler(this.cbMeasureType_SelectedIndexChanged);
             // 
             // MeasureEditForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(294, 261);
+            this.Controls.Add(this.cbMeasureType);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "MeasureEditForm";
             this.Text = "Create/Edit a measure...";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbMeasureType;
     }
 }
--- OneCharter/MeasureEditForm.cs
+++ OneCharter/MeasureEditForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RGData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,8 +11,36 @@ using System.Windows.Forms;
 
 namespace OneCharter {
     public partial class MeasureEditForm : Form {
+        /// <summary>false: creating, true: editing</summary>
+        protected bool isEditing;
+        public bool IsEditing {
+            get => isEditing;
+        }
+
+        /// <summary>The measure which is being edited.</summary>
+        protected Measure measure;
+        public Measure Measure {
+            get => measure;
+            set {
+                measure = value;
+                if(measure == null) {
+                    // Create a new measure.
+                    isEditing = false;
+                    // TODO: create a new default measure
+                } else {
+                    // Editing this measure... fill the values
+                    isEditing = true;
+                    // TODO: fill the values of controllers.
+                }
+            }
+        }
         public MeasureEditForm() {
             InitializeComponent();
+            cbMeasureType.SelectedIndex = 1;
+        }
+
+        private void cbMeasureType_SelectedIndexChanged(object sender, EventArgs e) {
+
         }
     }
 }
